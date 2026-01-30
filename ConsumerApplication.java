855d76b19760f90b550f1cd7972179a902a0c0eb
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-01-30 21:52:58 +0530

    Consumer interface with inner class

---
+++ ConsumerApplication.java
@@ -0,0 +1,21 @@
+import java.util.function.Consumer;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
+public class ConsumerApplication
+{
+	public static void main(String x[])
+	{
+		List<Integer> ls = new ArrayList<>();
+		ls.add(100);
+		ls.add(200);
+		ls.add(300);
+		Consumer<Integer> al = new Consumer<Integer>(){
+		public void accept(Integer num)
+		{
+			System.out.println(num);
+		}
+		};
+		ls.forEach(al);
+	}
+}
