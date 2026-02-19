852aa2e1d2d0fad0ab7696585d017bbd72079ed2
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-02-19 12:28:42 +0530

    display data using lambda and predicate interface

--- Predicate/PredicateApplicationApp.java
+++ Predicate/PredicateApplicationApp.java
@@ -7,21 +7,8 @@ public class PredicateApplicationApp
 	public static void main(String x[])
 	{
 		List<Integer> ls = List.of(10,11,20,23,22,45,44);
-		Predicate<Integer> pl = new Predicate<Integer>(){
-		public boolean test(Integer val)
-		{
-			return val % 2 == 0;
-		}
-		};
-		Consumer<Integer> cl = new Consumer<Integer>(){
-		public void accept(Integer val)
-		{
-			if(pl.test(val))
-			{
-				System.out.print(val+" ");
-			}
-		}
-		};
+		Predicate<Integer> pl = (val) -> val % 2 == 0;
+		Consumer<Integer> cl = (val) -> {if(pl.test(val)) System.out.print(val+" ");};
 		ls.forEach(cl);
 	}
 }
